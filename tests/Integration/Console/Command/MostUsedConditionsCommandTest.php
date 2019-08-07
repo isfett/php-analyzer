4568b05f7b19c33dcf5987e7eb8b376f311e970e
Christopher Stenke <christopher.stenke@statista.com>
2019-08-07 20:58:47 +0200

    updates

--- tests/Integration/Console/Command/MostUsedConditionsCommandTest.php
+++ tests/Integration/Console/Command/MostUsedConditionsCommandTest.php
@@ -14,8 +14,7 @@ use Isfett\PhpAnalyzer\Node\ProcessorRunner;
 use Isfett\PhpAnalyzer\Service\NodeRepresentationService;
 use Isfett\PhpAnalyzer\Service\SortService;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Input\ArrayInput;
++use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
@@ -60,7 +59,12 @@ class MostUsedConditionsCommandTest extends TestCase
     {
         $input = new ArrayInput([
             'most-used-conditions',
-            'directory' => __DIR__.'/../../../../tests/Unit/data',
+            'directory' => __DIR__ . '/../../../../tests/data',
+            '--include-files' => 'most_used_conditions_integrationtest.php',
+            '--visitors' => 'If,ElseIf,Ternary,Coalesce,BooleanReturn',
+            '--processors' => 'SplitIsset,SplitLogicalOperator,NegateBooleanNot,RemoveAssignment,RemoveCast,RemoveDuplicateBooleanNot,RemoveSingleFullyQualifiedName',
+            '--with-flip-check' => true,
+
         ], $this->mostUsedConditionsCommand->getDefinition());
 
         $output = new BufferedOutput();
@@ -72,6 +76,53 @@ class MostUsedConditionsCommandTest extends TestCase
             '<command-start>Starting most-used-conditions command</command-start>',
             $outputText
         );
-        $this->assertStringContainsString('<focus>', $outputText);
+
+        $expectedOutput = <<<EOT
++-------------------------------------------------------------------+-------+
+| Condition                                                         | Count |
++-------------------------------------------------------------------+-------+
+| <focus>\$_GET['page']</focus>                                      | 1     |
+| most_used_conditions_integrationtest.php:21 <flag></flag>         |       |
+|-------------------------------------------------------------------|-------|
+| <focus>\$i > 3</focus>                                             | 1     |
+| most_used_conditions_integrationtest.php:13 <flag></flag>         |       |
+|-------------------------------------------------------------------|-------|
+| <focus>\$someVar</focus>                                           | 1     |
+| most_used_conditions_integrationtest.php:11 <flag></flag>         |       |
+|-------------------------------------------------------------------|-------|
+| <focus>\$someVar !== 0</focus>                                     | 1     |
+| most_used_conditions_integrationtest.php:11 <flag></flag>         |       |
+|-------------------------------------------------------------------|-------|
+| <focus>\$this->getUser()</focus>                                   | 1     |
+| most_used_conditions_integrationtest.php:9 <flag></flag>          |       |
+|-------------------------------------------------------------------|-------|
+| <focus>\$x === \$y</focus>                                          | 1     |
+| most_used_conditions_integrationtest.php:25-26 <flag></flag>      |       |
+|-------------------------------------------------------------------|-------|
+| <focus>30 !== date('d')</focus>                                   | 1     |
+| most_used_conditions_integrationtest.php:15 <flag></flag>         |       |
+|-------------------------------------------------------------------|-------|
+| <focus>isset(\$_GET['page'])</focus>                               | 1     |
+| most_used_conditions_integrationtest.php:19 <flag></flag>         |       |
+|-------------------------------------------------------------------|-------|
+| <focus>isset(\$_SESSION['user'])</focus>                           | 1     |
+| most_used_conditions_integrationtest.php:3 <flag></flag>          |       |
+|-------------------------------------------------------------------|-------|
+| <focus>isset(\$_SESSION['user']['id'])</focus>                     | 1     |
+| most_used_conditions_integrationtest.php:3 <flag></flag>          |       |
+|-------------------------------------------------------------------|-------|
+| <focus>strtolower('Chris') === 'chris'</focus>                    | 1     |
+| most_used_conditions_integrationtest.php:17 <flag></flag>         |       |
+|-------------------------------------------------------------------|-------|
+| <focus>3771 === 1337</focus>                                      | 2     |
+| most_used_conditions_integrationtest.php:7 <flag></flag>          |       |
+| most_used_conditions_integrationtest.php:5 <flag>(flipped)</flag> |       |
+|-------------------------------------------------------------------|-------|
+| <focus>null === \$user</focus>                                     | 2     |
+| most_used_conditions_integrationtest.php:5 <flag></flag>          |       |
+| most_used_conditions_integrationtest.php:15 <flag></flag>         |       |
++-------------------------------------------------------------------+-------+
+EOT;
+        $this->assertStringContainsString($expectedOutput, $outputText);
     }
 }
