83d6ab9cffd92dc20b1d7ab22cbe8f3d4de58610
isfett <chris@isfett.com>
2019-08-15 19:49:47 +0200

    update examples

--- docs/examples/MagicNumberDetector/visitors.php
+++ docs/examples/MagicNumberDetector/visitors.php
@@ -1,1 +1,32 @@
 <?php
+
+class MagicNumberTestClass
+{
+    public const MAGICNUMBER = 3;
+
+    private $variable = 6;
+
+    public function test($input = 4)
+    {
+        if ($input > 2) {
+            return 15;
+        }
+
+        if (0 === $input) {
+            return  [
+                'test' => 12,
+            ];
+        }
+
+        switch ($input) {
+            case 8:
+                break;
+        }
+
+        $assignment = 26;
+
+        $rounded = round($this->variable, 17);
+
+        return 5;
+    }
+}
